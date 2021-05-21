75326ad52dd48cd2b5e7392221f794e4e19b60d7
Zavgorodneva Daria <zavgorodneva.daria@gmail.com>
2021-05-21 14:01:58 +0300

    HW1: were added task with program dialog (while section task 2)

--- learnpython_homeworks/task_4.py
+++ learnpython_homeworks/task_4.py
@@ -13,5 +13,20 @@ hello_user()
 
 
 # 2
-actions_list = {"Как дела": "Хорошо!", "Что делаешь?": "Программирую"}
+actions_list = {"Как дела?": "Хорошо!",
+                "Что делаешь?": "Программирую"}
 
+
+def user_ask(question_dict: dict):
+    user_question = ""
+    print("Введите вопрос. Для остановки введите Пока")
+    while user_question != "Пока":
+        user_question = input("Пользователь: ")
+        prog_answer = question_dict.get(user_question)
+        if user_question == "Пока":
+            print("Программа: И тебе пока!")
+        elif prog_answer is not None:
+            print(f"Программа: {prog_answer}")
+
+
+user_ask(actions_list)
