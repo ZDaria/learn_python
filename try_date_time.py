b3474572d4824fd043dd9e00af39aa3fd2b42854
Zavgorodneva Daria <zavgorodneva.daria@gmail.com>
2021-05-24 22:17:18 +0300

    HW1 were removed common files

--- try_date_time.py
+++
@@ -1,9 +0,0 @@
-import time
-import datetime
-
-
-today_date = datetime.date.today()
-conv_time = datetime.time(0, 0, 0)
-date_time = datetime.datetime.combine(today_date, conv_time)
-unix_date_time = date_time.strftime('%s')
-print(unix_date_time)
