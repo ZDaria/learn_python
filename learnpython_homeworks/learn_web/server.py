fd55cc05851bd664388fd65cb47f9125f27b1aae
Zavgorodneva Daria <zavgorodneva.daria@gmail.com>
2021-06-12 22:34:00 +0300

    HW4 HTML page were added template

--- learnpython_homeworks/learn_web/server.py
+++ learnpython_homeworks/learn_web/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
 
 app = Flask(__name__)
@@ -6,27 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    page_title = "Прогноз погоды"
     weather = weather_by_city("Moscow, Russia")
-    if weather:
-        weather_text = f"Погода: {weather['temp_C']} " \
-               f"Ощущается как: {weather['FeelsLikeC']}"
-    else:
-        weather_text = "Сервис погоды временно не доступен"
-    return f"""
-    <html>
-        <head>
-            <title>Прогноз погоды</title>  
-        </head>
-        <body>
-            <h1>{weather_text}</h1>
-            <ol>
-                <li>Один</li>
-                <li>Два</li>
-                <li>Три</li> 
-            </ol>
-        </body>
-    </html>
-    """
+    return render_template('index.html', page_title=page_title,
+                           weather=weather)
 
 
 if __name__ == "__main__":
