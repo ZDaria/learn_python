32dc01949ebad056b0ef25cc0d21a0c9bbd70161
Zavgorodneva Daria <zavgorodneva.daria@gmail.com>
2021-06-14 11:01:51 +0300

    HW4 were geted news from python.org were added to the web page as well

--- learnpython_homeworks/learn_web/server.py
+++ learnpython_homeworks/learn_web/server.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
 from weather import weather_by_city
+from python_org_news import get_python_news
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    page_title = "Новостной сайт"
+    page_title = "Новости Python"
     weather = weather_by_city("Moscow, Russia")
+    news_list = get_python_news()
+    print(news_list)
     return render_template('index.html', page_title=page_title,
-                           weather=weather)
+                           weather=weather, news_list=news_list)
 
 
 if __name__ == "__main__":
