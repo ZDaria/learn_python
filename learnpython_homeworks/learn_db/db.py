906dd792d49363d1fad73fb49523f6e1136bd3a7
Zavgorodneva Daria <zavgorodneva.daria@gmail.com>
2021-06-23 21:30:32 +0300

    HW4 - work with date base Adding, updating and deleting records at data base

--- learnpython_homeworks/learn_db/db.py
+++ learnpython_homeworks/learn_db/db.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-
-engine = create_engine("postgresql://mfkhtwul:UCu5TZs_LG4EiFD_iF-cW33_RJ49dKmO@"
-                       "hattie.db.elephantsql.com/mfkhtwul")
+from info import db_string
+engine = create_engine(db_string)
 db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
