2b8126ca651664ed45aa44c9f2cb44fc4df291ff
Zavgorodneva Daria <zavgorodneva.daria@gmail.com>
2021-07-14 21:10:00 +0300

    Were remover comments, empty lists, imports Refactor according to the pep 8

--- learn_web/webapp/forms.py
+++ learn_web/webapp/forms.py
@@ -4,6 +4,8 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Имя пользователя", validators=[DataRequired()], render_kw={"class": "form-control"})
-    password = PasswordField("Пароль",validators=[DataRequired()], render_kw={"class": "form-control"})
+    username = StringField("Имя пользователя", validators=[DataRequired()],
+                           render_kw={"class": "form-control"})
+    password = PasswordField("Пароль",validators=[DataRequired()],
+                             render_kw={"class": "form-control"})
     submit = SubmitField("Отправить", render_kw={"class": "btn btn-primary"})
