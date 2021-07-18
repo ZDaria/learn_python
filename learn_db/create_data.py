f8639b678311976922160f9d287f0c2bb8fb6c5e
Zavgorodneva Daria <zavgorodneva.daria@gmail.com>
2021-07-18 22:53:51 +0300

    DB refactor. Adding old code into separate dir

--- learn_db/create_data.py
+++ learn_db/create_data.py
@@ -1,23 +1,10 @@
-from faker import Faker
 import csv
 import random
 
+from faker import Faker
+
 fake = Faker('ru_RU')
 
-def get_fake_row():
-    return [fake.name(), fake.city_name(), fake.street_address(),
-            fake.large_company(), fake.job(), fake.phone_number(),
-            fake.free_email(), fake.date_of_birth(minimum_age=18,
-                                                  maximum_age=70),
-            random.randint(20000, 200000)]
+def fake_companies(num_rows = 10):
+    companies = []
 
-
-def generate_data(num_rows=100):
-    with open('salary.csv', 'w', encoding='utf-8') as f:
-        writer = csv.writer(f, delimiter=';')
-        for _ in range(num_rows):
-            writer.writerow(get_fake_row())
-
-
-if __name__ == '__main__':
-    generate_data()
